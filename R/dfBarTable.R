27cfbee139b8d76d0014b671d302887dedd92b0e
Johannes Karreth <jkarreth@ursinus.edu>
2019-07-13 11:14:22 -0400

    Add files via upload

---
+++ R/dfBarTable.R
@@ -0,0 +1,28 @@
+#' R function for summarizing a data frame as a table of bar plots
+#'@title Generate bar plots for multiple variables
+#'@description R function for summarizing a data frame as a table of bar plots
+#'@param rows number of rows of plots within the figure
+#'@return output
+#'@examples
+#' \donttest{
+#'   set.seed(123456)
+#'   example for user goes here
+#'   unit testing goes in testthat
+#' }
+#'@export
+
+dfBarTable <- function(df, rows = 2){
+
+require("ggplot2")
+
+plot <- ggplot(data = tidyr::gather(df, factor_key = TRUE), aes(x = factor(value))) + 
+  geom_bar() + 
+  facet_wrap(~ key, scales = "free", as.table = TRUE, nrow = rows) + 
+  scale_x_discrete(breaks = NULL) + 
+  scale_y_continuous(breaks = NULL) + 
+  xlab("") + 
+  ylab("") 
+
+return(plot)
+
+}
