e283f6ac3f5ba7adb35b57ca93b39fbb9708ef04
Shana Scogin <scogins@gmail.com>
2019-07-17 11:51:07 -0400

    switched test

--- tests/testthat/test_mcmcRocPrc.R
+++ tests/testthat/test_mcmcRocPrc.R
@@ -1,50 +1,39 @@
-test_that("Simple model runs with mcmcAveProb", {
-  
-  ## simulating data
-  set.seed(123456)
-  b0 <- 0.2 # true value for the intercept
-  b1 <- 0.5 # true value for first beta
-  b2 <- 0.7 # true value for second beta
-  n <- 500 # sample size
-  X1 <- runif(n, -1, 1)
-  X2 <- runif(n, -1, 1)
-  Z <- b0 + b1 * X1 + b2 * X2
-  pr <- 1 / (1 + exp(-Z)) # inv logit function
-  Y <- rbinom(n, 1, pr) 
-  data <- data.frame(cbind(X1, X2, Y))
-  
-  ## formatting the data for jags
-  datjags <- as.list(data)
-  datjags$N <- length(datjags$Y)
-  
-  ## creating jags model
-  model <- function()  {
-    
-    for(i in 1:N){
-      Y[i] ~ dbern(p[i])  ## Bernoulli distribution of y_i
-      logit(p[i]) <- mu[i]    ## Logit link function
-      mu[i] <- b[1] + 
-        b[2] * X1[i] + 
-        b[3] * X2[i]
-    }
-    
-    for(j in 1:3){
-      b[j] ~ dnorm(0, 0.001) ## Use a coefficient vector for simplicity
-    }
-    
-  }
-  
-  params <- c("b")
-  inits1 <- list("b" = rep(0, 3))
-  inits2 <- list("b" = rep(0, 3))
-  inits <- list(inits1, inits2)
-  
-  ## fitting the model with R2jags
-  set.seed(123)
-  fit <- R2jags::jags(data = datjags, inits = inits, 
-                      parameters.to.save = params, n.chains = 2, n.iter = 2000, 
-                      n.burnin = 1000, model.file = model)
-  
-  # ## running function
-  # object <- mcmcRocPrc(fit, ## what is model frame)
-})
+# test_that("Simple model runs with mcmcAveProb", {
+#   
+#   library("rstanarm")
+#   d <- carData::Cowles
+#   d$female <- (as.numeric(d$sex) - 2) * (-1)
+#   d$volunteer <- as.numeric(d$volunteer) - 1
+#   
+#   m <- stan_glm(volunteer ~ female + neuroticism + extraversion, 
+#                 data = d,
+#                 family = binomial(link = "logit"))
+#   
+#   # Using mcmcRocPrc
+#   
+#   # with median draws
+#   
+#   fit_sum <- mcmcRocPrc(sims = m,
+#                         modelframe = model.frame(m),
+#                         fullsims = FALSE)
+#   
+#   fit_sum$area_under_roc
+#   plot(x = fit_sum$roc_dat$x, y = fit_sum$roc_dat$y, type = "l", main = "ROC")
+#   abline(a = 0, b = 1)
+#   
+#   fit_sum$area_under_prc
+#   plot(x = fit_sum$prc_dat$x, y = fit_sum$prc_dat$y, type = "l", main = "PRC")
+#   
+#   # full draws
+#   
+#   fit_full <- mcmcRocPrc(sims = m,
+#                          modelframe = model.frame(m),
+#                          fullsims = TRUE)
+#   
+#   # works but this is slooooow!
+#   
+#   # area under roc:
+#   area_under_roc <- unlist(lapply(fit_full, '[[', 1))
+#   area_under_prc <- unlist(lapply(fit_full, '[[', 2))
+#   
+# })
