f79572c7a5abe19ca6af722b5b2e1e8cd46fc0dc
Andreas Beger <adbeger@gmail.com>
2019-09-02 14:34:16 +0300

    add line profiling script

---
+++ tests/mcmcRocPrc2.R
@@ -0,0 +1,10 @@
+
+skip("not a test")
+
+library("profvis")
+library("BayesPostEst")
+data("jags_logit")
+
+profvis(
+  BayesPostEst:::mcmcRocPrc2(jags_logit, "Y", c("X1", "X2"), curves = TRUE, fullsims = TRUE)
+)
