b9bfebd13ffe41dd0b2d22860fe45183b9bc98de
Rob Williams <jayrobwilliams@gmail.com>
2019-09-04 22:01:46 -0500

    add better sample extraction, adapt from stanfit list extraction to matrix extraction

--- R/mcmcme.R
+++ R/mcmcme.R
@@ -9,7 +9,7 @@
 #' interaction term.
 #' @param moderator a vector of values that the moderating parameter takes on
 #' in the data.
-#' @param point.est a character indicating whether to use the mean or median for
+#' @param pointest a character indicating whether to use the mean or median for
 #' point estimates in the plot.
 #' @param seq a numeric giving the number of moderator values used to generate
 #' the marginal effects plot.
@@ -21,26 +21,33 @@
 #' @export
 #'
 #' @examples
-mcmcme <- function(mod, main, int, moderator, point.est = 'mean', seq = 100,
-                   ci = .95, plot = T) {
+mcmcMargEff <- function(mod, main, int, moderator, pointest = 'mean', seq = 100,
+                        ci = .95, plot = T) {
 
-  ## check for stanfit
-  if (inherits(mod, 'stanfit')) {
-    ## coefficients for independent and interactive effect
-    samps <- rstan::extract(mod, pars = c(main, int))
+  ## pull in unexported functions from other packages
+  ## other options for future versions might include lifting this and adding authors as copr holders
+  runjags.as.mcmc.list.runjags = getFromNamespace("as.mcmc.list.runjags", "runjags")
+  if (inherits(mod, what = c("jags", "rjags"))) {
+    samps <- as.matrix(coda::as.mcmc(mod))
   }
-
-  ## check for brmsfit
-  if (inherits(mod, 'brmsfit')) {
-    ## coefficients for independent and interactive effect
-    samps <- rstan::extract(mod$fit, pars = c(main, int))
+  if (inherits(mod, what = "bugs")) {
+    samps <- mod$sims.matrix
   }
+  if (inherits(mod, what = "runjags")) {
+    samps <- as.matrix(runjags.as.mcmc.list.runjags(mod))
+  }
+  if (inherits(mod, what = c("mcmc", "mcmc.list", "stanfit", "stanreg",
+                             "brmsfit"))) {
+    samps <- as.matrix(mod)
+  }
+  
+  samps <- samps[, c(main, int)]
 
   ## expand moderating variable to range of values
   mod_range <- seq(min(moderator), max(moderator), length.out = seq)
 
   ## compute marginal effect for each sample
-  marg <- rep(samps[[1]], seq) + samps[[2]] %o% mod_range
+  marg <- rep(samps[, 1], seq) + samps[, 2] %o% mod_range
 
   ## calculate marginal effect for mean
   marg_mean <- apply(marg, 2, mean)
@@ -54,17 +61,14 @@ mcmcme <- function(mod, main, int, moderator, point.est = 'mean', seq = 100,
                         lo = marg_med[, 1], hi = marg_med[, 3])
 
   ## use mean for point estimate
-  if (point.est == 'mean') {
+  if (pointest == 'mean') {
 
     mep <- ggplot(data = marg_gg, aes(x = mod_range, y = mean, ymin = lo, ymax = hi)) +
       geom_ribbon(alpha = .25) +
       geom_hline(yintercept = 0, lty = 2, color = 'gray40', lwd = .5) +
       geom_line()
 
-  }
-
-  ## use median for point esimate
-  if (point.est == 'median') {
+  } else if (pointest == 'median') {
 
     mep <- ggplot(data = marg_gg, aes(x = mod_range, y = median, ymin = lo, ymax = hi)) +
       geom_ribbon(alpha = .25) +
