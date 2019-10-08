4b624048920cbf9064d92aca384c038f5a0ec71e
Rob Williams <jayrobwilliams@gmail.com>
2019-10-07 22:19:11 -0500

    add manual and tests for mcmcCoefPlot

---
+++ tests/testthat/test_mcmcCoefPlot.R
@@ -0,0 +1,19 @@
+test_that("Simple model runs with mcmcCoefPlot", {
+  
+  data("jags_logit")
+  fit <- jags_logit
+  
+  expect_silent(mcmcCoefPlot(fit))
+
+  expect_silent(mcmcCoefPlot(fit, pars = 'b'))
+  
+  expect_silent(mcmcCoefPlot(fit, pars = 'b'))
+  
+  med_df <- mcmcCoefPlot(fit, pars = 'b', pointest = 'median', plot = F)
+  value <- med_df[2, 1]
+  check_against <- 0.5273031
+  testthat::expect_equal(value, check_against, tolerance = 1e-4)
+
+})
+
+
